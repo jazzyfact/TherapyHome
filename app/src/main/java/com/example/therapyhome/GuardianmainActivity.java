a6978cace0b2c02a5f8a20ec4c8ac278f18de76d
byungop <byungop@naver.com>
2020-02-14 14:01:23 +0900

    화면 인텐트 연결

--- app/src/main/java/com/example/therapyhome/GuardianmainActivity.java
+++ app/src/main/java/com/example/therapyhome/GuardianmainActivity.java
@@ -2,13 +2,61 @@ package com.example.therapyhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GuardianmainActivity extends AppCompatActivity {
+
+    TextView bt_guardian_main;
+    TextView bt_guardian_txt;
+    TextView bt_guardian_num;
+    TextView bt_guardian_state;
+    TextView bt_guardian_msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guardianmain);
+
+        Button.OnClickListener onClickListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.bt_guardian_main:
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.bt_guardian_txt:
+                        Intent txt_intent = new Intent(getApplicationContext(), GuardiantexteditActivity.class);
+                        startActivity(txt_intent);
+                        break;
+                    case R.id.bt_guardian_num:
+                        Intent num_intent = new Intent(getApplicationContext(), GuardiannumActivity.class);
+                        startActivity(num_intent);
+                        break;
+                    case R.id.bt_guardian_state:
+                        Intent state_intent = new Intent(getApplicationContext(), GuardianstateActivity.class);
+                        startActivity(state_intent);
+                        break;
+                    case R.id.bt_guardian_msg:
+                        Intent msg_intent = new Intent(getApplicationContext(), GuardianmsgActivity.class);
+                        startActivity(msg_intent);
+                        break;
+                }
+            }
+        };
+        bt_guardian_main = findViewById(R.id.bt_guardian_main);
+        bt_guardian_main.setOnClickListener(onClickListener);
+        bt_guardian_txt = findViewById(R.id.bt_guardian_txt);
+        bt_guardian_txt.setOnClickListener(onClickListener);
+        bt_guardian_num = findViewById(R.id.bt_guardian_num);
+        bt_guardian_num.setOnClickListener(onClickListener);
+        bt_guardian_state = findViewById(R.id.bt_guardian_state);
+        bt_guardian_state.setOnClickListener(onClickListener);
+        bt_guardian_msg = findViewById(R.id.bt_guardian_msg);
+        bt_guardian_msg.setOnClickListener(onClickListener);
     }
 }
--- app/src/main/java/com/example/therapyhome/MainActivity.java
+++ app/src/main/java/com/example/therapyhome/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.therapyhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt_controlIoT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_controlIoT = findViewById(R.id.bt_controlIoT);
+        bt_controlIoT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ControliotActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
