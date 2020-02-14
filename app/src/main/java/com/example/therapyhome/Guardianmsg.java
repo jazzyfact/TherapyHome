f729ccb2b898d6e63740cc5a941ae576ffa886b4
byungop <byungop@naver.com>
2020-02-14 13:02:43 +0900

    1. DoctermsgActivity - 리사이클러뷰 생성.
    2. GuardianmsgActivity
    - 리사이클러뷰 생성

---
+++ app/src/main/java/com/example/therapyhome/Guardianmsg.java
@@ -0,0 +1,14 @@
+package com.example.therapyhome;
+
+public class Guardianmsg {
+
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
