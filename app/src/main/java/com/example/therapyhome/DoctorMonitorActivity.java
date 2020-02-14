6bfd7a1aa7c5b2b117a7d817ebf6cd41aa6d5ca4
jazzyfact <jazzyfact2000@gmail.com>
2020-02-15 01:57:29 +0900

    DoctorMonitorActivity 심박, 호흡 1씩 스레드 증가 구현

--- app/src/main/java/com/example/therapyhome/DoctorMonitorActivity.java
+++ app/src/main/java/com/example/therapyhome/DoctorMonitorActivity.java
@@ -1,15 +1,73 @@
 package com.example.therapyhome;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DoctorMonitorActivity extends AppCompatActivity {
 
+
+    private int bpm = 80;
+    private int co2 = 95;
+    private TextView tvBpmData;
+    private TextView tvCo2mData;
+    private TextView tvTempertureData;
+    private TextView tvWaterData;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_monitor);
+
+        tvBpmData = findViewById(R.id.tv_bpmdata);
+        tvCo2mData = findViewById(R.id.tv_co2mdata);
+        tvTempertureData = findViewById(R.id.tv_temperturedata);
+        tvWaterData = findViewById(R.id.tv_waterdata);
+
+
+    }
+
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            updateThread();
+            updateCo2Thread();//호흡
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Thread myThread = new Thread(new Runnable() {
+            public void run() {
+                while (true) {
+                    try {
+                        handler.sendMessage(handler.obtainMessage());
+                        Thread.sleep(1000);
+                    } catch (Throwable t) {
+                    }
+                }
+            }
+        });
+        myThread.start();
+    }
+
+    private void updateThread() {
+        bpm++;
+        tvBpmData.setText(String.valueOf(bpm));
+
+
+
+    }
+
+    private void updateCo2Thread(){
+        co2 ++;
+        tvCo2mData.setText(String.valueOf(co2));
     }
 }
