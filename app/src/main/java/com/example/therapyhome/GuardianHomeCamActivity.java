a58b271a798c47540e9e91a3cf464142339024f9
jazzyfact <jazzyfact2000@gmail.com>
2020-02-19 00:04:36 +0900

    GuardianHomeCameActivity 생성 후 webView 기능 추가

---
+++ app/src/main/java/com/example/therapyhome/GuardianHomeCamActivity.java
@@ -0,0 +1,51 @@
+package com.example.therapyhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class GuardianHomeCamActivity extends AppCompatActivity {
+
+
+    private WebView webView;
+    private WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guardian_home_cam);
+        webView = findViewById(R.id.web_view);
+
+        webView.setWebViewClient(new WebViewClient());
+        webSettings = webView.getSettings();
+
+
+        webView.setPadding(0, 0, 0, 0);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+
+
+        //webView.loadUrl("http://192.168.0.2:8090/stream_simple.html");
+        webView.loadUrl("http://192.168.0.2:8090/javascript_simple.html");
+
+
+      
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
