a6070f19b7562889327068755223a6e297db127d
jazzyfact <jazzyfact2000@gmail.com>
2020-02-21 23:48:13 +0900

    GuardianHomeCameActivity 생성 후 webView 기능 추가 수정 바텀네비게이션 달음

--- app/src/main/java/com/example/therapyhome/GuardianHomeCamActivity.java
+++ app/src/main/java/com/example/therapyhome/GuardianHomeCamActivity.java
@@ -2,17 +2,20 @@ package com.example.therapyhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class GuardianHomeCamActivity extends AppCompatActivity {
 
 
     private WebView webView;
     private WebSettings webSettings;
-
+    Button btnEditKeyword, btnEditPhone, btnCheckHealth, btnReadMsg, btnHomeCam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,18 +26,57 @@ public class GuardianHomeCamActivity extends AppCompatActivity {
         webSettings = webView.getSettings();
 
 
-        webView.setPadding(0, 0, 0, 0);
+        webView.setPadding(0,0,0,0);
+        //webView.setInitialScale(100);
         webView.getSettings().setBuiltInZoomControls(false);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
 
-
         //webView.loadUrl("http://192.168.0.2:8090/stream_simple.html");
-        webView.loadUrl("http://192.168.0.2:8090/javascript_simple.html");
+       // webView.loadUrl("http://192.168.0.9:8090/javascript_simple.html");
 
+        //String url ="http://192.168.0.9:8090/javascript_simple.html";
+        String url = "http://192.168.0.9:8090/?action=stream";
+        webView.loadUrl(url);
 
-      
+        //하단 네비게이션바
+        btnEditKeyword = findViewById(R.id.bt_edit_keyword); //키워드 편집 버튼
+        btnEditPhone = findViewById(R.id.bt_edit_phone);//연락처 편집 버튼
+        btnCheckHealth = findViewById(R.id.bt_check_health);//건강 상태 버튼
+        btnReadMsg = findViewById(R.id.bt_read_msg);//문자 모아보기 버튼
+        Button.OnClickListener onClickListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.bt_read_msg: //문자모아보기
+                        Intent intent = new Intent(getApplicationContext(), GuardianMainActivity.class);
+                        startActivity(intent);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                        break;
+                    case R.id.bt_edit_keyword: //키워드 편집
+                        Intent editKeywordIntent = new Intent(getApplicationContext(), GuardianKeywordEditActivity.class);
+                        startActivity(editKeywordIntent);
+                        editKeywordIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        break;
+                    case R.id.bt_edit_phone://연락처 편집
+                        Intent editPhomeIntent = new Intent(getApplicationContext(), GuardianPhoneActivity.class);
+                        startActivity(editPhomeIntent);
+                        editPhomeIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        break;
+                    case R.id.bt_check_health://환자 건강 상태
+                        Intent checkHealthIntent = new Intent(getApplicationContext(), GuardianMonitorActivity.class);
+                        startActivity(checkHealthIntent);
+                        checkHealthIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        break;
+                }
+            }
+        };
+        btnReadMsg.setOnClickListener(onClickListener);
+        btnCheckHealth.setOnClickListener(onClickListener);
+        btnEditKeyword.setOnClickListener(onClickListener);
+        btnEditPhone.setOnClickListener(onClickListener);
     }
 
     @Override
